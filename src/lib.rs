e9ad29a88d3b05c70c7d309f166847df8da0872d
Leinad Roczak <daniel.kac94@gmail.com>
2024-10-02 11:11:05 +0200

    Remove unused
    Remove unused imports.

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,7 @@ static TEST_AUTH_TOKEN: &'static str = "user";
 use serde::{Deserialize, Serialize};
 #[allow(unused)]
 use std::sync::{Mutex, MutexGuard, OnceLock};
-use {
-    anyhow, anyhow::Result, async_trait, futures::future::BoxFuture, lazy_static::lazy_static,
-    mockito, mockito::Server, reqwest, serde_json, tokio,
-};
+use {anyhow, futures::future::BoxFuture, lazy_static::lazy_static, reqwest};
 
 // Clients
 lazy_static! {
