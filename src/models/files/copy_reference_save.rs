5f7ab4055653c2755d91ee14bc65c5e99ca673d9
Leinad Roczak <daniel.kac94@gmail.com>
2024-10-09 12:20:49 +0200

    Add copy reference
    Added copy reference models

---
+++ src/models/files/copy_reference_save.rs
@@ -0,0 +1,69 @@
+use super::{SaveCopyReferenceArgs as Args, SaveCopyReferenceResult as RequestResult};
+
+use crate::{
+    anyhow::Result,
+    endpoints::headers::Headers,
+    endpoints::{get_endpoint_url, Endpoint},
+    errors::ApiError,
+    implement_service, implement_utils,
+    traits::{Service, Utils},
+    AsyncClient, BoxFuture, SyncClient,
+};
+use serde::Deserialize;
+use std::{future::Future, pin::Pin};
+
+/// Type aliases for readability
+type Request<'a> = SaveReferenceGetRequest<'a>;
+type Response = SaveReferenceGetResponse;
+type RequestPayload = Args;
+type ResponsePayload = RequestResult;
+
+/// Copy
+/// https://www.dropbox.com/developers/documentation/http/documentation#files-copy_reference-save
+#[derive(Debug)]
+pub struct SaveReferenceGetRequest<'a> {
+    pub access_token: &'a str,
+    pub payload: Option<RequestPayload>,
+}
+
+/// Response struct for adding properties
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+pub struct SaveReferenceGetResponse {
+    pub payload: ResponsePayload,
+}
+
+// Impl utils trait
+implement_utils!(Request<'_>, RequestPayload);
+
+// Impl service trait
+implement_service!(
+    Request<'_>,
+    Response,
+    ResponsePayload,
+    Endpoint::FilesCopyReferenceSavePost,
+    vec![Headers::ContentTypeAppJson]
+);
+
+#[cfg(test)]
+mod tests {
+    use crate::TEST_AUTH_TOKEN;
+
+    use super::{Request, RequestPayload};
+
+    use tokio;
+
+    use crate::{
+        endpoints::{get_endpoint_url, headers::Headers, Endpoint},
+        implement_tests,
+        tests_utils::{get_endpoint_test_body_response, get_mut_or_init, get_mut_or_init_async},
+        traits::Service,
+    };
+
+    implement_tests!(
+        Endpoint::FilesCopyReferenceSavePost,
+        vec![Headers::TestAuthorization, Headers::ContentTypeAppJson],
+        Request,
+        RequestPayload
+    );
+}
