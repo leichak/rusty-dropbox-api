f1d8128926adb5aa5ef6d00ec066e47b43116daf
Leinad Roczak <daniel.kac94@gmail.com>
2024-11-12 22:00:10 +0100

    Add upload session start
    Add upload session start model.

--- src/models/files/upload_session_start.rs
+++ src/models/files/upload_session_start.rs
@@ -1,1 +1,77 @@
 
+use super::{FileMetadata as Args, UploadSessionStartArg as RequestResult};
+
+use crate::{
+    anyhow::Result,
+    endpoints::headers::Headers,
+    endpoints::{get_endpoint_url, Endpoint},
+    errors::ApiError,
+    implement_service, implement_utils,
+    traits::{Service, Utils},
+    AsyncClient, BoxFuture, SyncClient,
+};
+use serde::Deserialize;
+use std::{future::Future, pin::Pin};
+
+/// Type aliases for readability
+type Request<'a> = UploadSessionStartRequest<'a>;
+type Response = UploadSessionStartResponse;
+type RequestPayload = Args;
+type ResponsePayload = RequestResult;
+
+/// UploadSessionStart
+/// https://www.dropbox.com/developers/documentation/http/documentation#files-UploadSessionStart
+#[derive(Debug)]
+pub struct UploadSessionStartRequest<'a> {
+    pub access_token: &'a str,
+    pub payload: Option<RequestPayload>,
+}
+
+/// Response struct for adding properties
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+pub struct UploadSessionStartResponse {
+    pub payload: ResponsePayload,
+}
+
+// Impl utils trait
+implement_utils!(Request<'_>, RequestPayload);
+
+// Impl service trait
+implement_service!(
+    Request<'_>,
+    Response,
+    ResponsePayload,
+    Endpoint::FilesUploadSessionStartPost,
+    vec![
+        Headers::ContentTypeAppOctetStream("".to_string()),
+        Headers::DropboxApiArg("".to_string())
+    ]
+);
+
+#[cfg(test)]
+mod tests {
+    use crate::TEST_AUTH_TOKEN;
+
+    use super::{Request, RequestPayload};
+
+    use tokio;
+
+    use crate::{
+        endpoints::{get_endpoint_url, headers::Headers, Endpoint},
+        implement_tests,
+        tests_utils::{get_endpoint_test_body_response, get_mut_or_init, get_mut_or_init_async},
+        traits::Service,
+    };
+
+    implement_tests!(
+        Endpoint::FilesUploadSessionStartPost,
+        vec![
+            Headers::TestAuthorization,
+            Headers::ContentTypeAppOctetStream("".to_string()),
+            Headers::DropboxApiArg("".to_string()),
+        ],
+        Request,
+        RequestPayload
+    );
+}
